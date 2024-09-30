7a017e55f51e00a069019041328c8645df9f2b56
UJJWAL SHARMA <92508362+ujjwal0-2@users.noreply.github.com>
2024-09-30 13:16:10 +0530

    Create database.js

---
+++ database.js
@@ -0,0 +1,28 @@
+// database in form of npm i mongodb
+const { MongoClient } = require('mongodb');
+// or as an es module:
+// import { MongoClient } from 'mongodb'
+
+// Connection URL
+const url =" mongodb+srv://UjjwalSharma:itxI6bCWVSSLSGuB@cluster0.rhwnd.mongodb.net/";
+const client = new MongoClient(url);
+
+// Database Name
+const dbName = 'myProject';
+
+async function main() {
+  // Use connect method to connect to the server
+  await client.connect();
+  console.log('Connected successfully to server');
+  const db = client.db(dbName);
+  const collection = db.collection('documents');
+
+  // the following code examples can be pasted here...
+
+  return 'done.';
+}
+
+main()
+  .then(console.log)
+  .catch(console.error)
+  .finally(() => client.close());
